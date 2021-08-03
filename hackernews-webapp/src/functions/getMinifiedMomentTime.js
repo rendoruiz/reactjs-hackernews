149af48e93407a98748638b64cd7e331448c5d46
rendoruiz <rendo.ruiz@outlook.com>
2021-08-03 17:30:53 -0600

    update moment time minification

--- hackernews-webapp/src/functions/getMinifiedMomentTime.js
+++ hackernews-webapp/src/functions/getMinifiedMomentTime.js
@@ -1,27 +1,18 @@
 const getMinifiedMomentTime = (momentString) => {
   const minified = momentString
-    .replace('a year ago', '1y')
-    .replace('year ago', 'y')
-    .replace('years ago', 'y')
-    .replace('a month ago', '1m')
-    .replace('month ago', 'm')
-    .replace('months ago', 'm')
-    .replace('a week ago', '1w')
-    .replace('week ago', 'w')
-    .replace('weeks ago', 'w')
-    .replace('a day ago', '1d')
-    .replace('day ago', 'd')
-    .replace('days ago', 'd')
-    .replace('an hour ago', '1h')
-    .replace('hour ago', 'h')
-    .replace('hours ago', 'h')
-    .replace('aminute ago', '1m')
-    .replace('minute ago', 'm')
-    .replace('minutes ago', 'm')
-    .replace('a second ago', '1s')
-    .replace('second ago', 's')
     .replace('a few seconds ago', '0m')
-    .replace(' ', '');
+    .replace('a ', '1')
+    .replace('an ', '1')
+    .replace('s ', '')
+    .replace('ago', '')
+    .replace('year', 'y')
+    .replace('month', 'm')
+    .replace('week', 'w')
+    .replace('day', 'd')
+    .replace('hour', 'h')
+    .replace('minute', 'm')
+    .replace('second', 's')
+    .replaceAll(' ', '');
   return minified;
 }
 
