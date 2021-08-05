5a96b534e0754ba43b93444b638e0815d2e5257e
rendoruiz <rendo.ruiz@outlook.com>
2021-08-05 10:49:10 -0600

    netlify spa redirect fix & website meta update

--- hackernews-webapp/src/App.js
+++ hackernews-webapp/src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import StoryCatalogView from './containers/StoryCatalogView';
 import StoryDetailView from './containers/StoryDetailView';
 import UserDetailView from './containers/UserDetailView';
