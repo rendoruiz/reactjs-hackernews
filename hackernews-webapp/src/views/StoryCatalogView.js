5760800ea3cfc8b18a1189ffa8d9037860f3f257
rendoruiz <rendo.ruiz@outlook.com>
2021-08-08 00:30:22 -0600

    category story orders

--- hackernews-webapp/src/views/StoryCatalogView.js
+++ hackernews-webapp/src/views/StoryCatalogView.js
@@ -1,39 +1,102 @@
 import { useState, useEffect } from "react";
 import ContentItemStoryCard from "../components/ContentItemStoryCard";
-import { getTopStoryIds } from "../functions/hackernewsApi";
+import { getStoryIdList } from "../functions/hackernewsApi";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link, useHistory, useParams } from "react-router-dom";
+import { faBurn, faCertificate, faChartLine } from "@fortawesome/free-solid-svg-icons";
 
 const StoryCatalogView = () => {
   // const [itemCount, setItemCount] = useState(20);
   const itemCount = 20;
-  // const [itemOrder, setItemOrder] = useState('top');
+  const { order } = useParams();
+  const [itemOrder, setItemOrder] = useState(null);
   const [storyItemIdList, setStoryItemIdList] = useState(null);
+  const history = useHistory();
 
   useEffect(() => {
     // setTimeout(() => {
-      getTopStoryIds().then((data) => {
-        setStoryItemIdList(data);
-        document.title = "Top Stories - Readit News";
-        // console.log(data);
-      });
-    // }, 1000);
-  }, []);
 
-  // const handleOrderTypeClick = ({ e, order }) => {
-  //   e.preventDefault();
-  //   // setItemOrder(order);
-  //   console.log(this.type);
-  // }
+      if (order === 'best') {
+        setDocumentTitle('Best');
+        setItemOrder('best');
+        getStoryIdList('best').then((data) => {
+          setStoryItemIdList(data);
+        });
+        console.log('loading best')
+      } 
+      else if (order === 'new') {
+        setDocumentTitle('New');
+        setItemOrder('new');
+        getStoryIdList('new').then((data) => {
+          setStoryItemIdList(data);
+        });
+        console.log('loading new')
+      } 
+      else if (order === 'top' || !order) {
+        setDocumentTitle('Top');
+        setItemOrder('top');
+        getStoryIdList('top').then((data) => {
+          setStoryItemIdList(data);
+        });
+        console.log('loading top')
+      } 
+      else {
+        history.push(`/`);
+      }
+
+    // }, 1000);
+  }, [order, history]);
+
+  const setDocumentTitle = (type) => {
+    document.title = `${type} Stories - Readit News`;
+  }
+
+  const setActiveButton = (type) => {
+    if (type === itemOrder) {
+      console.log('qq')
+      return ' active';
+    }
+  }
 
   return ( 
     <div className="page catalog">
       <section className="stories">
+        <header className="navigation-group">
+          <Link 
+            className={`navigation-item ` + setActiveButton('top')}
+            to={`/top`}
+            title="Top Stories from Hacker News"
+          >
+            <FontAwesomeIcon icon={faChartLine} className="glyph" />
+            <span>Top Story</span>
+          </Link>
+          <Link 
+            className={`navigation-item ` + setActiveButton('best')}
+            to={`/best`}
+            title="Best Stories from Hacker News"
+          >
+            <FontAwesomeIcon icon={faBurn} className="glyph" />
+          <span>Best Story</span>
+          </Link>
+          <Link 
+            className={`navigation-item ` + setActiveButton('new')}
+            to={`/new`}
+            title="New Stories from Hacker News"
+          >
+            <FontAwesomeIcon icon={faCertificate} className="glyph" />
+            <span>New Story</span>
+          </Link>
+        </header> 
+
+
         {/* <div className="stories-actions">
           <button className="btn" onClick={(e) => handleOrderTypeClick({e, order: 'top'})}>Top</button>
           <button className="btn" onClick={(e) => handleOrderTypeClick({e, order: 'best'})}>Best</button>
           <button className="btn" onClick={(e) => handleOrderTypeClick({e, order: 'new'})}>New</button>
         </div> */}
         
-        { !storyItemIdList ? <span>Loading stories...</span> : storyItemIdList.slice(0, itemCount).map((itemId) => <ContentItemStoryCard key={itemId} itemId={itemId} /> ) } <ContentItemStoryCard />
+        { !storyItemIdList ? <span>Loading stories...</span> : storyItemIdList.slice(0, itemCount).map((itemId) => <ContentItemStoryCard key={itemId} itemId={itemId} /> ) } 
       </section>
       <aside className="sidebar">
 
