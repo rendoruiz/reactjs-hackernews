dd896c9b29fa84f5d9641e743d0b6601834b6218
rendoruiz <rendo.ruiz@outlook.com>
2021-08-20 19:03:24 -0600

    site header + css modules start

--- hackernews-webapp/src/components/SiteHeader.js
+++ hackernews-webapp/src/components/SiteHeader.js
@@ -1,6 +1,21 @@
+import { useHistory } from 'react-router';
+import styles from '../styles/SiteHeader.module.css'
+import logo from '../images/logo30.png'
+
 const SiteHeader = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => {
+    history.replace('/');
+  }
+
   return ( 
-    <div className="site-header">Readit News!</div> 
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.logoImage} src={logo} alt="" srcset="" />
+        <span className={styles.logoText} onClick={handleOnClick}>Readit News</span>
+      </div>
+    </header> 
   );
 }
  
