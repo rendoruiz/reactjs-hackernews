4620c55435f4f5f43e694802dd63f0dc57331ebd
rendoruiz <rendo.ruiz@outlook.com>
2021-08-24 17:17:44 -0600

    add classname prop

--- hackernews-webapp/src/components/Links/UserViewLink.js
+++ hackernews-webapp/src/components/Links/UserViewLink.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
-const UserViewLink = ({ userId, isText = false }) => {
+const UserViewLink = ({ userId, isText = false, className }) => {
   return !userId ? null : ( 
     <Link
       to={"/u/" + userId}
       title="Go to user page"
+      className={className}
     >
       { (!isText ? "u/" : "") + userId }
     </Link>
