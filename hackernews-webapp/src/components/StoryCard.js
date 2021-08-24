246ffc2cd4d75b4d2edd1cb4191d492d6ecefb49
rendoruiz <rendo.ruiz@outlook.com>
2021-08-24 17:01:58 -0600

    Update StoryCard.js

--- hackernews-webapp/src/components/StoryCard.js
+++ hackernews-webapp/src/components/StoryCard.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { faCommentAlt } from '@fortawesome/free-regular-svg-icons';
 import { faHackerNewsSquare } from '@fortawesome/free-brands-svg-icons';
 
