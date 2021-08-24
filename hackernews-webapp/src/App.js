4f398ab6ce80b2fe4b990505921a2106f0bb7752
rendoruiz <rendo.ruiz@outlook.com>
2021-08-24 11:40:49 -0600

    app css modules

--- hackernews-webapp/src/App.js
+++ hackernews-webapp/src/App.js
@@ -4,29 +4,32 @@ import NotFoundView from './views/NotFoundView';
 import CatalogView from './views/CatalogView';
 import StoryView from './views/StoryView';
 import UserView from './views/UserView';
+import styles from './styles/App.module.css';
 
 function App() {
   return (
-    <Router>
-      <SiteHeader />
-      <div className="content">
-        <Switch>
-        <Redirect from='/top' to='/' />
-          <Route exact path={["/", "/best", "/new"]}>
-            <CatalogView />
-          </Route>
-          <Route exact path={["/u/:userId/", "/u/:userId/story", "/u/:userId/comment"]}>
-            <UserView />
-          </Route>
-          <Route path="/s/:id">
-            <StoryView />
-          </Route>
-          <Route path={["*", "/404"]}>
-            <NotFoundView />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className={styles.container}>
+      <Router>
+        <SiteHeader />
+        <div className={styles.content}>
+          <Switch>
+          <Redirect from='/top' to='/' />
+            <Route exact path={["/", "/best", "/new"]}>
+              <CatalogView />
+            </Route>
+            <Route exact path={["/u/:userId/", "/u/:userId/story", "/u/:userId/comment"]}>
+              <UserView />
+            </Route>
+            <Route path="/s/:id">
+              <StoryView />
+            </Route>
+            <Route path={["*", "/404"]}>
+              <NotFoundView />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
