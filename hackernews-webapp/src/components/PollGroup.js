aa0f168ebd42e5c9b7b3980c20528a588ea4d320
rendoruiz <rendo.ruiz@outlook.com>
2021-08-24 20:03:11 -0600

    add incomplete poll support

---
+++ hackernews-webapp/src/components/PollGroup.js
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Poll.module.css'
+
+const PollGroup = () => {
+  return ( 
+    <span className={styles.warning}>Polls are not supported</span>
+  );
+}
+ 
+export default PollGroup;
